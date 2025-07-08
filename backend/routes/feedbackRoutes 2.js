b22b03f7f5a9eeda15d421c9dd4b38367c8d22da
Aditya Pratap Singh <adityapratapsingh2803@gmail.com>
2025-07-08 19:53:31 +0530

    vapis daldiya code

---
+++ backend/routes/feedbackRoutes 2.js
@@ -0,0 +1,8 @@
+// routes/feedbackRoutes.js
+const express = require('express');
+const router = express.Router();
+const { submitFeedback } = require('../controllers/feedbackController');
+
+router.post('/', submitFeedback);
+
+module.exports = router;
